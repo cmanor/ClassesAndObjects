e2717b0a55a4998cea9110113e47f7cf93937c74
cmanor <101116379+cmanor@users.noreply.github.com>
2022-05-12 17:48:51 -0400

    fjeiwao

--- src/main/java/org/example/BankAccountApp.java
+++ src/main/java/org/example/BankAccountApp.java
@@ -16,14 +16,16 @@ public class BankAccountApp {
         accounts.add(new BankAccount("fake3", 696969, 2000));
 
         System.out.println("WHAT!?");
-        System.out.println("Are you an existing customer? (-1 to exit) \n 1. Yes\n 2. No");
 
-        int response = Integer.valueOf(scan.nextLine());
 
-        while(response != -1) {
+        int response;
+
+        while(true) {
+            System.out.println("Are you an existing customer? (-1 to exit) \n 1. Yes\n 2. No");
+            response = Integer.parseInt(scan.nextLine());
             if (response == 1) {
                 System.out.println("What is your account number?");
-                int accountNum = Integer.valueOf(scan.nextLine());
+                int accountNum = Integer.parseInt(scan.nextLine());
 
                 boolean isAccountHolder = false;
                 int index = -1;
@@ -51,7 +53,7 @@ public class BankAccountApp {
                 String responseName = scan.nextLine();
                 newAccount.setAccountHolderName(responseName);
 
-                double responseAmount = Double.valueOf(scan.nextLine());
+                double responseAmount = Double.parseDouble(scan.nextLine());
                 newAccount.setAccountBalance(responseAmount);
 
                 accounts.add(newAccount);
@@ -74,37 +76,39 @@ public class BankAccountApp {
 
         System.out.println("Hello " + acc.getAccountHolderName());
 
-        System.out.println("Welcome to the main menu, what would you like to do today?");
-        System.out.println(" 1. To check account balance \n 2. To make a withdrawal \n 3. To make a deposit \n 4. To make a transfer to another account \n 0. To Exit");
 
-        int response = Integer.valueOf(scan.nextLine());
 
-        while (response != 0) {
+        int response;
+
+        while (true) {
+            System.out.println("Welcome to the main menu, what would you like to do today?");
+            System.out.println(" 1. To check account balance \n 2. To make a withdrawal \n 3. To make a deposit \n 4. To make a transfer to another account \n 0. To Exit");
+            response = Integer.parseInt(scan.nextLine());
             if (response == 1) {
-                acc.getAccountBalance();
+                System.out.println("Account balance is " + acc.getAccountBalance());
 
             } else if (response == 2) {
-                double withdrawAmount = Double.valueOf(scan.nextLine());
+                double withdrawAmount = Double.parseDouble(scan.nextLine());
                 acc.withdrawal(withdrawAmount);
                 System.out.println("The amount: " + withdrawAmount + " has been taken out.");
 
             } else if (response == 3) {
-                double depositAmount = Double.valueOf(scan.nextLine());
+                double depositAmount = Double.parseDouble(scan.nextLine());
                 acc.deposit(depositAmount);
                 System.out.println("The amount: " + depositAmount + " has been added to your account.");
 
             } else if (response == 4) {
                 System.out.println("Please enter the account number to transfer to ");
-                int accountToTransferTo = Integer.valueOf(scan.nextLine());
+                int accountToTransferTo = Integer.parseInt(scan.nextLine());
 
                 BankAccount toTransferTo = new BankAccount();
                 boolean isAccountHolder = false;
                 int index = -1;
 
                 for (int i = 0; i < accounts.size(); i++) {
-                    BankAccount accs = accounts.get(i);
-                    if (accountToTransferTo == accs.getAccountNumber()) {
-                        toTransferTo = accs;
+                    BankAccount acc2 = accounts.get(i);
+                    if (accountToTransferTo == acc2.getAccountNumber()) {
+                        toTransferTo = acc2;
                         isAccountHolder = true;
                         index = i;
                     }
@@ -112,7 +116,7 @@ public class BankAccountApp {
 
                 if (isAccountHolder) {
                     System.out.println("How much would you like to transfer ");
-                    double transferAmount = Double.valueOf(scan.nextLine());
+                    double transferAmount = Double.parseDouble(scan.nextLine());
 
                     acc.transfer(toTransferTo, transferAmount);
                     System.out.println(acc.getAccountHolderName() + " moved " + transferAmount + " and now has " + acc.getAccountBalance());
